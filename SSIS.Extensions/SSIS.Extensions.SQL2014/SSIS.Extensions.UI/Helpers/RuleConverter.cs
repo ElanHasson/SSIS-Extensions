c9eed08b10959a26b1b78d95771684cd8a9ccc71
Elan Hasson <elan@elanhasson.com>
2016-03-16 17:50:05 -0400

    Initial commit of 2014 changes.

---
+++ SSIS.Extensions/SSIS.Extensions.SQL2014/SSIS.Extensions.UI/Helpers/RuleConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace SSIS.Extensions.UI
+{
+    public class RuleConverter : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            //true means show a combobox
+            return true;
+        }
+
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            //true will limit to list. false will show the list, 
+            //but allow free-form entry
+            return false;
+        }
+
+        public override System.ComponentModel.TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(UserVariables.ListOfUserVariables);
+        }
+    }
+}
---
+++ SSIS.Extensions/SSIS.Extensions/Helpers/Enums.cs
@@ -0,0 +1,70 @@
+﻿
+using System.ComponentModel;
+
+namespace SSIS.Extensions
+{
+    public enum PGPFileAction
+    {
+        [Description("Encrypt")]
+        Encrypt,
+
+        [Description("Encrypt & Sign")]
+        EncryptAndSign,
+
+        [Description("Decrypt")]
+        Decrypt
+    }
+
+    public enum SFTPFileAction
+    {
+        [Description("Send File(s)")]
+        Send,
+
+        [Description("Send Mutiple Files")]
+        SendMultiple,
+
+        [Description("Receive File(s)")]
+        Receive,
+
+        [Description("Receive Mutiple Files")]
+        ReceiveMultiple,
+
+        [Description("Remote File List")]
+        List
+    }
+
+    public enum ZipFileAction
+    {
+        Compress,
+        Decompress
+    }
+
+    public enum CompressionType
+    {
+        Zip,
+        Tar
+    }
+
+    public enum ZipCompressionLevel
+    {
+        Ultra=9,
+        Maximum=6,
+        Normal=4,
+        Minimum=2,
+        None=0
+    }
+
+    public enum TarCompressionLevel
+    {
+        BZip2,
+        GZip,
+        None
+    }
+
+    public enum LogLevel
+    {
+        Verbose = 3,
+        Minimal = 2,
+        None = 1
+    }
+}
